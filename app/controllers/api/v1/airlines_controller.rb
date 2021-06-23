6481ceecf7996ac505aab2373b2dc84f97853e5b
Raven <rhold.navarette@kodakollectiv.com>
2021-06-23 11:30:24 +0800

    protect from forgery

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class AirlinesController < ApplicationController
+      protect_from_forgery with: :null_session
+
       def index
         airlines = Airline.all
 
@@ -10,14 +12,14 @@ module Api
       def show
         airline = Airline.find_by(slug: params[:slug])
 
-        render json: AirlineSerializer.new(airlines, options).serialized_json
+        render json: AirlineSerializer.new(airline, options).serialized_json
       end
 
       def create
         airline = Airline.new(airline_params)
 
         if airline.save
-          render json: AirlineSerializer.new(airlines).serialized_json
+          render json: AirlineSerializer.new(airline).serialized_json
         else
           render json: { error: airline.errors.messages }, status: 422
         end
@@ -27,7 +29,7 @@ module Api
         airline = Airline.find_by(slug: params[:slug])
 
         if airline.update(airline_params)
-          render json: AirlineSerializer.new(airlines, options).serialized_json
+          render json: AirlineSerializer.new(airline, options).serialized_json
         else
           render json: { error: airline.errors.messages }, status: 422
         end
