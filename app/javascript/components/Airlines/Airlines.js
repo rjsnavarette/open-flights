817160cf90d1fc9b86a5706fbbfd8526970d2acd
Raven <rhold.navarette@kodakollectiv.com>
2021-06-23 15:09:52 +0800

    add airlines index react js

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+import Airline from './Airline'
 
 const Airlines = () => {
-  return <div>This is the Airlines#index view for our app.</div>
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+    // Get all of our airlines from api
+    // Update airlines in our state
+
+    axios.get('/api/v1/airlines.json')
+    .then( resp => {
+      setAirlines(resp.data.data)
+    })
+    .catch( resp => console.log(resp) )
+  }, [airlines.length])
+
+  const grid = airlines.map( item => {
+    return (
+      <Airline 
+        key={item.attributes.name}
+        attributes={item.attributes}
+      />
+    )
+  })
+
+  return (
+    <div className="home">
+      <div className="header">
+        <h1>OpenFlights</h1>
+        <div className="subheader">Honest, unbiased airline reviews.</div>
+      </div>
+      <div className="grid">
+        {grid}
+      </div>
+    </div>
+  )
 }
 
 export default Airlines
